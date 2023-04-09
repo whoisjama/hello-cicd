7dba8165542958f4b1997b69f25be5c72fc170d1
18821795980 <Jama1314>
2023-04-09 10:50:06 +0800

    The POC for CICD

---
+++ src/main/java/per/yhq/HiDockerApplication.java
@@ -0,0 +1,11 @@
+package per.yhq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiDockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HiDockerApplication.class, args);
+	}
+}
